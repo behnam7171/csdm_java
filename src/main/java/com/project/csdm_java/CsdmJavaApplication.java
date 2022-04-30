49e812ee47649980a08439c0d74b402fecd1a7e0
Behnam <behnam.bozorgi71@gmail.com>
2022-04-30 17:08:44 +0200

    Initial commit

---
+++ src/main/java/com/project/csdm_java/CsdmJavaApplication.java
@@ -0,0 +1,13 @@
+package com.project.csdm_java;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CsdmJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CsdmJavaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/project/csdm_java/CsdmJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.csdm_java;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CsdmJavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
