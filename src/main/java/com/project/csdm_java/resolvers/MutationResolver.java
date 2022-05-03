edc14d39fd5c5a188b2344af1dc20b4350bfdf0b
Behnam <behnam.bozorgi71@gmail.com>
2022-05-03 17:32:40 +0200

    comment MutationResolver.java

--- src/main/java/com/project/csdm_java/resolvers/MutationResolver.java
+++ src/main/java/com/project/csdm_java/resolvers/MutationResolver.java
@@ -32,11 +32,10 @@ public class MutationResolver implements GraphQLMutationResolver {
         // find the authors
         List<Author> authors = authorRepository.findAllById(authorsIds);
 
-        // book.setAuthors(Set.copyOf(authors));
         bookRepository.save(book);
 
         for (Author author : authors) {
-            // did not check for null because findAllById never returns null if id was not found
+            // did not check for null because findAllById (on few lines above) never returns null if id was not found
             // it just returns the authors that exists in the list of ids
             book.addAuthor(author);
         }
@@ -58,6 +57,7 @@ public class MutationResolver implements GraphQLMutationResolver {
     public Book updateBook(Long id, String title, Genre genre, List<Long> authorsIds) throws ObjectNotFoundException {
         Optional<Book> currentBook = bookRepository.findById(id);
         if (this.bookExists(id)) {
+            // user might only change one of the attributes so we need to check if it is passed through the api or not.
             Book book = currentBook.get();
             if (title != null) {
                 book.setTitle(title);
