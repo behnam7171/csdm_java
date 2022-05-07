9e946f03e8b41d1efb9f8c37ad5a6f768460c785
Behnam <behnam.bozorgi71@gmail.com>
2022-05-07 12:42:06 +0200

    Fixing n+1 problem by entitiyGraph

--- src/main/java/com/project/csdm_java/repositories/BookRepository.java
+++ src/main/java/com/project/csdm_java/repositories/BookRepository.java
@@ -1,7 +1,21 @@
 package com.project.csdm_java.repositories;
 
 import com.project.csdm_java.models.Book;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface BookRepository extends JpaRepository<Book, Long> {
+    @EntityGraph(
+            type = EntityGraph.EntityGraphType.FETCH,
+            attributePaths = {
+                    "authors",
+                    "authors.firstName",
+                    "authors.lastName",
+                    "authors.dateOfBirth"
+            }
+    )
+    List<Book> findAll();
+
 }
